03525beb95fef191e33c960d16f44e86a48af40c
su <suxuefei110@qq.com>
2015-10-07 21:54:53 +0800

    修改

--- 看知/看知/AppDelegate.m
+++ 看知/看知/AppDelegate.m
@@ -21,7 +21,7 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor blackColor];
     [self.window makeKeyAndVisible];
-    
+    NSString* str = @"这只是一个修改";
     MainTabBarViewController *mtvc = [[MainTabBarViewController alloc] init];
     self.window.rootViewController = mtvc;
     
